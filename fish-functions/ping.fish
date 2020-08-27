280b2f51afa6d8c974dc791e7c9d56e496ca37bd
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2020-08-27 23:28:11 +0200

    Remove functions shadowed by abbreviations

--- fish-functions/ping.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function ping --wraps='prettyping --nolegend' --description 'alias ping=prettyping --nolegend'
-  prettyping --nolegend $argv;
-end
--- fish-functions/pr.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function pr --wraps='hub pull-request' --description 'alias pr=hub pull-request'
-  hub pull-request $argv;
-end
