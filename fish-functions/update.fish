cd49d997e681d4d25b2831f31707ad2c4d7c5d82
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2021-01-15 17:33:49 +0100

    fish: fix update function
    Remove redundant Homebrew cask stuff since everything is handled by brew
    update/upgrade now.
    Simplified fisher updating.

--- fish-functions/update.fish
+++ fish-functions/update.fish
@@ -4,10 +4,9 @@
 #
 function update
     # Get macOS Software Updates; update Homebrew, npm, fisher, and their installed packages
-    sudo softwareupdate -i -a &&
-    echo -e "\n-----------------\nUPDATING HOMEBREW\n-----------------\n"; brew update; brew upgrade &&
-    echo -e "\n--------------\nUPDATING CASKS\n--------------\n"; brew cu -a &&
+    echo -e "\n-----------------------\nUPDATING APPLE SOFTWARE\n-----------------------\n"; sudo softwareupdate -i -a &&
+    echo -e "\n-----------------\nUPDATING HOMEBREW\n-----------------\n"; brew update && brew upgrade &&
     echo -e "\n------------\nBREW CLEANUP\n------------\n"; brew cleanup &&
-    echo -e "\n----------\nNPM UPDATE\n----------\n"; npm install npm -g; npm update -g &&
-    echo -e "\n---------------------\nUPDATING FISH PLUGINS\n---------------------\n"; fisher self-update; fisher
+    echo -e "\n----------\nNPM UPDATE\n----------\n"; npm install npm -g && npm update -g &&
+    echo -e "\n---------------------\nUPDATING FISH PLUGINS\n---------------------\n"; fisher update
 end
