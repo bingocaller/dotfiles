4c3adbf1ac76e04ac7371544a666df2069d2043d
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2023-01-21 11:35:54 +0100

    Tweak update script
    **Why:**
    
    I wanted to move the order around so Apple software updates get
    installed last to avoid restarting in the middle of an update. Also, I
    wanted to make things a bit more readable
    
    **What:**
    
    * Move order of update scripts around.
    * Reformat to improve readability.

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -2,11 +2,21 @@
 # Helper Functions
 # http://apple.stackexchange.com/a/96810/7647
 #
+# Update Homebrew, npm, fisher, and their installed packages; get macOS Software
+# Updates.
 function update
-    # Get macOS Software Updates; update Homebrew, npm, fisher, and their installed packages
-    echo -e "\n-----------------------\nUPDATING APPLE SOFTWARE\n-----------------------\n"; sudo softwareupdate -i -a &&
-    echo -e "\n-----------------\nUPDATING HOMEBREW\n-----------------\n"; brew update && brew upgrade &&
-    echo -e "\n------------\nBREW CLEANUP\n------------\n"; brew cleanup &&
-    echo -e "\n----------\nNPM UPDATE\n----------\n"; npm-check -gu &&
-    echo -e "\n---------------------\nUPDATING FISH PLUGINS\n---------------------\n"; fisher update
+	echo -e "\n-----------------\nUPDATING HOMEBREW\n-----------------\n";
+	brew update && brew upgrade;
+	echo -e "\n----------------\nHOMEBREW CLEANUP\n----------------\n";
+	brew cleanup;
+
+	echo -e "\n----------\nNPM UPDATE\n----------\n";
+	npm-check -gu;
+
+	echo -e "\n---------------------\nUPDATING FISH PLUGINS\n---------------------\n";
+	fisher update;
+
+	# Update Apple software last, since this can sometimes entail a restart.
+	echo -e "\n-----------------------\nUPDATING APPLE SOFTWARE\n-----------------------\n";
+	sudo softwareupdate -i -a;
 end
