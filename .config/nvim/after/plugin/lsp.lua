5d31a45dd2c39771d12697e032972740913030c8
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2023-09-03 12:42:48 +0200

    Neovim: Improve lsp-zero setup

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,27 +1,51 @@
-local lsp = require("lsp-zero")
-
--- Use recommended LSP Zero config but override sign icons
--- lsp.preset("recommended")
-lsp.set_preferences({
-	suggest_lsp_servers = true,
-	setup_servers_on_start = true,
-	set_lsp_keymaps = true,
-	configure_diagnostics = true,
-	cmp_capabilities = true,
-	manage_nvim_cmp = true,
-	call_servers = "local",
-	sign_icons = {
-		error = "",
-		warn = "",
-		hint = "",
-		info = "",
+local lspconfig = require("lspconfig")
+local lsp_zero = require("lsp-zero").preset({
+	name = "recommended",
+	set_lsp_keymaps = {
+		omit = { "gs" },
 	},
 })
 
--- Configure lua language server for Neovim
-lsp.nvim_workspace()
+lsp_zero.on_attach(function(_client, bufnr)
+	-- lsp_zero.default_keymaps({ buffer = bufnr })
+	lsp_zero.buffer_autoformat()
+end)
 
-lsp.setup()
+lsp_zero.set_sign_icons({
+	error = "",
+	warn = "",
+	hint = "",
+	info = "",
+})
+
+-- Skip setup of rust-analyzer. rust-tools handles that for us.
+lsp_zero.skip_server_setup({ "rust_analyzer" })
+
+-- Kept here for reference; we might need to use this instead of
+-- buffer_autoformat() to avoid Prettier wreaking havoc on files that have
+-- better LSPs/formatters available.
+-- lsp_zero.format_on_save({
+-- 	servers = {
+-- 		["lua_ls"] = { "lua" },
+-- 		["rust_analyzer"] = { "rust" },
+-- 		["stylelint_lsp"] = { "css", "scss", "sass" },
+-- 	},
+-- })
+
+-- Configure lua language server for Neovim
+lspconfig.lua_ls.setup(lsp_zero.nvim_lua_ls())
+
+-- Set stylelint to fix formatting on format. This will happen on save because
+-- we've set up formatting on save (lsp.buffer_autoformat()).
+lspconfig.stylelint_lsp.setup({
+	settings = {
+		stylelintplus = {
+			autoFixOnFormat = true,
+		},
+	},
+})
+
+lsp_zero.setup()
 
 -- Use virtual text to display diagnostics
 -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/main/advance-usage.md#configure-errors-messages
@@ -29,30 +53,31 @@ vim.diagnostic.config({
 	virtual_text = true,
 })
 
--- https://github.com/VonHeikemen/lsp-zero.nvim/blob/main/advance-usage.md#intergrate-with-null-ls
--- https://github.com/jay-babu/mason-null-ls.nvim#primary-source-of-truth-is-mason-null-ls
-local mason_null_ls = require("mason-null-ls")
-mason_null_ls.setup({
+-- LSP-specific setups
+
+-- We skipped setup of rust-analyzer because rust-tools handles that for us
+require("rust-tools").setup()
+
+-- null-ls setup
+
+local null_ls = require("null-ls")
+null_ls.setup({
+	sources = {
+		-- Replace these with the tools you want to install
+		-- null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.stylua,
+	},
+})
+require("mason-null-ls").setup({
+	ensure_installed = nil,
 	automatic_installation = true,
 	automatic_setup = true,
+	-- handlers = {
+	-- Here you can add functions to register sources.
+	-- See https://github.com/jay-babu/mason-null-ls.nvim#handlers-usage
+	--
+	-- If left empty, mason-null-ls will  use a "default handler"
+	-- to register all sources
+	-- }
 })
-
--- https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save#sync-formatting
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-require("null-ls").setup({
-	-- you can reuse a shared lspconfig on_attach callback here
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
-})
-
-mason_null_ls.setup_handlers()
