421f6e434d565502d63d1813a0b3f517e466f5ac
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2023-09-03 12:44:13 +0200

    Neovim: Tweak Telescope setup, install extensions
    **Why:**
    
    I wanted to use the Ivy layout for most pickers and I wanted to try the
    zoxide and gh extensions.
    
    **What:**
    
    * Set Ivy layout on most pickers.
    * Install gh and zoxide extensions and add a keymap for zoxide.
    * Add `<C-f>` keymap for `builtin.grep_string`.

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,19 +3,28 @@ local builtin = require("telescope.builtin")
 
 telescope.setup({
 	defaults = {
-		prompt_prefix = "❯ ",
+		prompt_prefix = "󰍉 ",
 		selection_caret = "› ",
 	},
 
 	pickers = {
+		git_files = {
+			theme = "ivy",
+		},
 		grep_string = {
 			hidden = true,
+			theme = "ivy",
 		},
 		find_files = {
 			hidden = true,
+			theme = "ivy",
 		},
 		live_grep = {
 			hidden = true,
+			theme = "ivy",
+		},
+		oldfiles = {
+			theme = "ivy",
 		},
 		spell_suggest = {
 			theme = "cursor",
@@ -28,10 +37,17 @@ telescope.setup({
 
 telescope.load_extension("fzf")
 telescope.load_extension("ui-select")
+telescope.load_extension("gh")
+telescope.load_extension("zoxide")
 
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<C-h>", builtin.oldfiles, {})
 vim.keymap.set("n", "<C-b>", builtin.buffers, {})
 vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
+-- Mnemonic: Project Search
 vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+vim.keymap.set("n", "<C-f>", builtin.grep_string, {})
+
 vim.keymap.set("n", "=z", builtin.spell_suggest, {})
+
+vim.keymap.set("n", "<leader>cd", telescope.extensions.zoxide.list)
