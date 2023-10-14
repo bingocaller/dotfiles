39266fc9fec69d86b5642ee67352009f40578ef7
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2023-10-14 11:03:58 +0200

    Tweak treesitter setup, format comments

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -15,6 +15,7 @@ require("nvim-treesitter.configs").setup({
 		"git_rebase",
 		"gitcommit",
 		"gitignore",
+		"help",
 		"html",
 		"javascript",
 		"jsdoc",
@@ -38,24 +39,20 @@ require("nvim-treesitter.configs").setup({
 		"vim",
 		"yaml",
 	},
-
-	-- Install parsers synchronously (only applied to `ensure_installed`)
+	-- Install parsers asynchronously (only applied to `ensure_installed`)
 	sync_install = false,
-
 	-- Automatically install missing parsers when entering buffer
 	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 	auto_install = true,
-
 	-- List of parsers to ignore installing (for "all")
 	-- ignore_install = { "javascript" },
 
-	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+	-- If you need to change the installation directory of the parsers (see -> Advanced Setup)
 	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
 	highlight = {
 		-- `false` will disable the whole extension
 		enable = true,
-
 		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
 		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
 		-- the name of the parser)
@@ -80,18 +77,15 @@ require("nvim-treesitter.configs").setup({
 				node_decremental = "grm",
 			},
 		},
-
 		-- Indentation based on treesitter for the = operator. NOTE: This is an experimental feature.
 		indent = {
 			enable = true,
 		},
-
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
-
 		-- Plugins
 		-- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
 		context_commentstring = {
