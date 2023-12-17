f270e4bfb2445c41f5e5c31f4e22f506cc1e7fe9
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2023-12-17 13:58:30 +0100

    Neovim: tweak ZenMode settings
    **Why:**
    
    Enabling ZenMode would disable visible whitespace characters and
    `scrolloff` settings.
    
    **What:**
    
    Explicitly set `list` and `scrolloff` options in ZenMode setup.

--- .config/nvim/after/plugin/zen-mode.lua
+++ .config/nvim/after/plugin/zen-mode.lua
@@ -6,7 +6,7 @@ require("zen-mode").setup({
 		-- * a percentage of the width / height of the editor when <= 1
 		-- * a function that returns the width or the height
 		width = 80, -- width of the Zen window
-		height = 1, -- height of the Zen window
+		-- height = 1, -- height of the Zen window
 		-- by default, no options are changed for the Zen window
 		-- uncomment any of the options below, or add other vim.wo options you want to apply
 		options = {
@@ -16,7 +16,8 @@ require("zen-mode").setup({
 			cursorline = false, -- disable cursorline
 			cursorcolumn = false, -- disable cursor column
 			foldcolumn = "0", -- disable fold column
-			list = false, -- disable whitespace characters
+			list = true,   -- enable whitespace characters
+			scrolloff = 8,
 		},
 	},
 })
