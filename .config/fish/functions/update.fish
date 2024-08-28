e9134c78d62352ee91a54188443286a2e5f98f86
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2024-08-28 11:04:02 +0200

    Fish: update script tweaks
    **Why:**
    
    * Fisher would sometimes fail to update when `fish` had been updated by
      Homebrew.
    * Sometimes updates would behave strangely and/or fail when running from
      other directories than home.
    * Updating Neovim would take a long time because we would wipe the
      directory and install from a completely clean slate every time.
    
    **What:**
    
    * Reorder update scripts so fisher runs first.
    * Jump to home directory before installing any updates, then jump back
      to the previous directory when install has finished.
    * Run `make clean distclean` in Neovim project before installing instead
      of wiping the entire directory first.

--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -2,43 +2,51 @@
 # Helper Functions
 # http://apple.stackexchange.com/a/96810/7647
 #
-# Update Homebrew, npm, fisher, and their installed packages; get macOS Software
+# Update fish plugins, Homebrew, Neovim, npm packages, and get macOS Software
 # Updates.
 function update
-	echo -e "╭───────────────────╮"
-	echo -e "│ UPDATING HOMEBREW │"
-	echo -e "╰───────────────────╯";
-	brew update && brew upgrade;
-	echo -e "╭──────────────────╮"
-	echo -e "│ HOMEBREW CLEANUP │"
-	echo -e "╰──────────────────╯";
-	brew cleanup;
+    # Store working directory so we can come back later (`cd -` may not always
+    # work).
+    set ORIGINAL_DIRECTORY (pwd)
+    cd ~
 
-	echo -e "╭─────────────────╮"
-	echo -e "│ UPDATING NEOVIM │"
-	echo -e "╰─────────────────╯";
-	cd ~/dev;
-	# For some reason the install only works on a fresh clone
-	rm -rf neovim;
-	git clone https://github.com/neovim/neovim;
-	cd neovim;
-	make CMAKE_BUILD_TYPE=RelWithDebInfo;
-	sudo make install;
-	cd ~;
+    # Update Fish plugins first to avoid failures when Fish is upgraded as part
+    # of `brew upgrade`
+    echo -e "╭───────────────────────╮"
+    echo -e "│ UPDATING FISH PLUGINS │"
+    echo -e "╰───────────────────────╯"
+    fisher update
 
-	echo -e "╭────────────╮"
-	echo -e "│ NPM UPDATE │"
-	echo -e "╰────────────╯";
-	npm-check -gu;
+    echo -e "╭───────────────────╮"
+    echo -e "│ UPDATING HOMEBREW │"
+    echo -e "╰───────────────────╯"
+    brew update && brew upgrade
+    echo -e "╭──────────────────╮"
+    echo -e "│ HOMEBREW CLEANUP │"
+    echo -e "╰──────────────────╯"
+    brew cleanup
 
-	echo -e "╭───────────────────────╮"
-	echo -e "│ UPDATING FISH PLUGINS │"
-	echo -e "╰───────────────────────╯";
-	fisher update;
+    echo -e "╭─────────────────╮"
+    echo -e "│ UPDATING NEOVIM │"
+    echo -e "╰─────────────────╯"
+    cd ~/dev/neovim
+    git pull
+    make clean distclean
+    make CMAKE_BUILD_TYPE=RelWithDebInfo
+    sudo make install
+    cd ~
 
-	# Update Apple software last, since this can sometimes entail a restart.
-	echo -e "╭─────────────────────────╮"
-	echo -e "│ UPDATING APPLE SOFTWARE │"
-	echo -e "╰─────────────────────────╯";
-	sudo softwareupdate -i -a;
+    echo -e "╭────────────╮"
+    echo -e "│ NPM UPDATE │"
+    echo -e "╰────────────╯"
+    npm-check -gu
+
+    # Update Apple software last, since this can sometimes entail a restart.
+    echo -e "╭─────────────────────────╮"
+    echo -e "│ UPDATING APPLE SOFTWARE │"
+    echo -e "╰─────────────────────────╯"
+    sudo softwareupdate -i -a
+
+    # Return to original directory
+    cd $ORIGINAL_DIRECTORY
 end
