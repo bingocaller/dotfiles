fbc0ffacbd64eb5897e802a9a7acbd5b6fef11d5
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2024-09-06 13:04:04 +0200

    Tweak symlinks setup
    **Why:**
    
    I was doing a lot of one-to-one symlinking of stuff inside `~/.config`
    instead of linking the entire directory.
    
    **What:**
    
    * Symlink all of `.config` instead of individual directories within it.
    * Sort commands alphabetically within each section.
    * Comment out config symlink for now.

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,23 +1,15 @@
 #!/usr/bin/env bash
 
 # ~
-ln -s ~/dev/dotfiles/bash/.bash_profile ~/.bash_profile &&
-ln -s ~/dev/dotfiles/bash/.bashrc ~/.bashrc &&
-ln -s ~/dev/dotfiles/git/.git_commit_message ~/.git_commit_message &&
-ln -s ~/dev/dotfiles/git/.gitconfig ~/.gitconfig &&
-ln -s ~/dev/dotfiles/git/.gitignore_global ~/.gitignore_global &&
-ln -s ~/dev/dotfiles/.hammerspoon ~/.hammerspoon &&
-ln -s ~/dev/dotfiles/.stylelintrc.json ~/.stylelintrc.json &&
-ln -s ~/dev/dotfiles/.config/tmux/.tmux.conf ~/.tmux.conf &&
-ln -s ~/dev/dotfiles/vim/.vim ~/.vim &&
-ln -s ~/dev/dotfiles/vim/.vimrc ~/.vimrc &&
+ln -s ~/dev/dotfiles/.hammerspoon ~/.hammerspoon
+ln -s ~/dev/dotfiles/.stylelintrc.json ~/.stylelintrc.json
+ln -s ~/dev/dotfiles/git/.git_commit_message ~/.git_commit_message
+ln -s ~/dev/dotfiles/git/.gitconfig ~/.gitconfig
+ln -s ~/dev/dotfiles/git/.gitignore_global ~/.gitignore_global
+ln -s ~/dev/dotfiles/svgo.config.js ~/svgo.config.js
+ln -s ~/dev/dotfiles/vim/.vim ~/.vim
+ln -s ~/dev/dotfiles/vim/.vimrc ~/.vimrc
 
 # ~/.config
-ln -s ~/dev/dotfiles/.config/bat ~/.config/bat &&
-ln -s ~/dev/dotfiles/.config/fish ~/.config/fish &&
-ln -s ~/dev/dotfiles/.config/htop ~/.config/htop &&
-ln -s ~/dev/dotfiles/.config/karabiner ~/.config/karabiner &&
-ln -s ~/dev/dotfiles/.config/kitty ~/.config/kitty &&
-ln -s ~/dev/dotfiles/.config/nvim ~/.config/nvim &&
-ln -s ~/dev/dotfiles/.config/spicetify ~/.config/spicetify &&
-ln -s ~/dev/dotfiles/.config/tmux ~/.config/tmux
+ln -s ~/dev/dotfiles/.config ~/.config
+# ln -s ~/dev/dotfiles/.config/tmux/.tmux.conf ~/.tmux.conf
